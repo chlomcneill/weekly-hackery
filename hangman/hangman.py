b4a0922bd8c23e08b9f99b3cc3c619975c5f64e8
chlomcneill <c.mcneill@elsevier.com>
2019-06-05 12:09:23 +0100

    Got rid of 'already guessed' dialogue

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -8,7 +8,7 @@ f.close()
 
 
 def random_word():
-    selection = randint(0, len(list(dictionary)))
+    selection = randint(0, len(list(dictionary))-1)
     choice = dictionary[selection].strip()
     return choice
 
@@ -17,15 +17,10 @@ def guessing(target):
     blanks = list('-' * len(target))
     print(target)
     print(' '.join(blanks))
-    guesses = []
     bad_guesses = 0
     while blanks != list(target):
         if bad_guesses < 6:
-            guess = input("Guess a letter: ")
-            if guess in guesses:
-                print("You've already guessed that!")
-                guess = input("Guess again: ")
-            guesses += guess
+            guess = input("You have " + str(6 - bad_guesses) + " chances left. Guess a letter: ")
             if target.count(guess) > 0:
                 indices = [i for i, a in enumerate(target) if a == guess]
                 for index in indices:
@@ -45,7 +40,7 @@ def hangman():
     if play:
         print("Congratulations! The word was: " + target)
     else:
-        print("You have run out of guesses. The word was: " + target)
+        print("You have run out of chances. The word was: " + target)
 
 
 hangman()
