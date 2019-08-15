5ba97f47b6d6be933810d794121130e5017a73aa
chlomcneill <c.mcneill@elsevier.com>
2019-08-15 11:35:38 +0100

    scoring file created, finds each player's best hand after flop

---
+++ texasholdem/scoring.py
@@ -0,0 +1,158 @@
+ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
+
+
+def get_hand_ranks(hand):
+    hand_ranks = []
+    for card in hand:
+        hand_ranks.append(card[0])
+    return hand_ranks
+
+
+def get_hand_suits(hand):
+    hand_suits = []
+    for card in hand:
+        hand_suits.append(card[1])
+    return hand_suits
+
+
+def is_straight(hand):
+    hand_ranks = get_hand_ranks(hand)
+    hand_ranks.sort(key=lambda x: ranks.index(x))
+    if ranks.index(hand_ranks[1]) == ranks.index(hand_ranks[0])+1 and ranks.index(hand_ranks[2]) == ranks.index(hand_ranks[1])+1 and ranks.index(hand_ranks[3]) == ranks.index(hand_ranks[2])+1 and ranks.index(hand_ranks[4]) == ranks.index(hand_ranks[3])+1:
+        return True
+    elif hand_ranks == ['Ace', '10', 'Jack', 'Queen', 'King']:
+        return True
+    elif hand_ranks == ['Ace', '2', 'Jack', 'Queen', 'King']:
+        return True
+    elif hand_ranks == ['Ace', '2', '3', 'Queen', 'King']:
+        return True
+    elif hand_ranks == ['Ace', '2', '3', '4', 'King']:
+        return True
+    else:
+        return False
+
+
+def is_flush(hand):
+    hand_suits = get_hand_suits(hand)
+    if all(x == hand_suits[0] for x in hand_suits):
+        return True
+    else:
+        return False
+
+
+def contains_only_one_pair(hand):
+    hand_ranks = get_hand_ranks(hand)
+    count = [hand_ranks.count(rank) for rank in list(dict.fromkeys(hand_ranks))]
+    count.sort()
+    # print(count)
+    if count == [1, 1, 1, 2]:
+        return True
+    else:
+        return False
+
+
+def contains_three_of_a_kind(hand):
+    hand_ranks = get_hand_ranks(hand)
+    count = [hand_ranks.count(rank) for rank in list(dict.fromkeys(hand_ranks))]
+    count.sort()
+    # print(count)
+    if count == [1, 1, 3]:
+        return True
+    else:
+        return False
+
+
+def contains_two_pairs(hand):
+    hand_ranks = get_hand_ranks(hand)
+    count = [hand_ranks.count(rank) for rank in list(dict.fromkeys(hand_ranks))]
+    count.sort()
+    # print(count)
+    if count == [1, 2, 2]:
+        return True
+    else:
+        return False
+
+
+def is_full_house(hand):
+    hand_ranks = get_hand_ranks(hand)
+    count = [hand_ranks.count(rank) for rank in list(dict.fromkeys(hand_ranks))]
+    count.sort()
+    # print(count)
+    if count == [2, 3]:
+        return True
+    else:
+        return False
+
+
+def contains_four_of_a_kind(hand):
+    hand_ranks = get_hand_ranks(hand)
+    count = [hand_ranks.count(rank) for rank in list(dict.fromkeys(hand_ranks))]
+    count.sort()
+    # print(count)
+    if count == [1, 4]:
+        return True
+    else:
+        return False
+
+
+def is_straight_flush(hand):
+    if is_flush(hand) and is_straight(hand):
+        return True
+    else:
+        return False
+
+
+def is_royal_flush(hand):
+    hand_ranks = get_hand_ranks(hand)
+    royal_flush = ['Ace', 'King', 'Queen', 'Jack', '10']
+    if set(hand_ranks) == set(royal_flush) and is_flush(hand):
+        return True
+    else:
+        return False
+
+
+def high_card(hand):
+    hand_ranks = get_hand_ranks(hand)
+    hand_ranks.sort(key=lambda x: ranks.index(x))
+    if hand_ranks[0] == 'Ace':
+        return 'Ace'
+    else:
+        return hand_ranks[-1]
+
+
+def score_hand(hand):
+    if is_royal_flush(hand):
+        return 10, None
+    elif is_straight_flush(hand):
+        return 9, high_card(hand)
+    elif contains_four_of_a_kind(hand):
+        return 8, high_card(hand)
+    elif is_full_house(hand):
+        return 7, high_card(hand)
+    elif is_flush(hand):
+        return 6, high_card(hand)
+    elif is_straight(hand):
+        return 5, high_card(hand)
+    elif contains_three_of_a_kind(hand):
+        return 4, high_card(hand)
+    elif contains_two_pairs(hand):
+        return 3, high_card(hand)
+    elif contains_only_one_pair(hand):
+        return 2, high_card(hand)
+    else:
+        return 1, high_card(hand)
+
+
+# test = [('Ace', 'Diamonds'), ('2', 'Diamonds'), ('3', 'Diamonds'), ('King', 'Diamonds'), ('Queen', 'Hearts')]
+# print(is_straight(test))
+# print(is_flush(test))
+# print(is_straight_flush(test))
+# print(contains_only_one_pair(test))
+# print(contains_three_of_a_kind(test))
+# print(contains_two_pairs(test))
+# print(is_full_house(test))
+# print(contains_four_of_a_kind(test))
+# print(is_royal_flush(test))
+# print(score_hand(test))
+
--- texasholdem/texas_hold_em.py
+++ texasholdem/texas_hold_em.py
@@ -1,54 +1,106 @@
 import random
 import itertools
+from texasholdem.scoring import score_hand
 
 
-def deck():
-    ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
-    suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
-    cards = list(itertools.product(ranks, suits))
-    return cards
+ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
 
 
 def deal_a_card(deck):
-    card = deck.pop(random.randint(0, len(deck)))
+    card = deck.pop(random.randint(0, len(deck)-1))
     return card, deck
 
 
 def discard_a_card(deck):
-    deck.pop(random.randint(0, len(deck)))
+    deck.pop(random.randint(0, len(deck)-1))
     return deck
 
 
 def deal_to_players(num_of_players):
-    cards = deck()
+    deck = list(itertools.product(ranks, suits))
     hands = []
     for i in range(1, num_of_players + 1):
-        first_card, cards = deal_a_card(cards)
-        second_card, cards = deal_a_card(cards)
+        first_card, deck = deal_a_card(deck)
+        second_card, deck = deal_a_card(deck)
         hands.append([i, first_card, second_card])
-    return hands, cards
+    return hands, deck
 
 
-def deal_to_table(cards):
+def flop(deck):
     table = []
-    cards = discard_a_card(cards)
+    deck = discard_a_card(deck)
     for i in range(3):
-        card, cards = deal_a_card(cards)
+        card, deck = deal_a_card(deck)
         table.append(card)
-    cards = discard_a_card(cards)
-    card, cards = deal_a_card(cards)
+    return table, deck
+
+
+def turn(table, deck):
+    deck = discard_a_card(deck)
+    card, deck = deal_a_card(deck)
     table.append(card)
-    cards = discard_a_card(cards)
-    card, cards = deal_a_card(cards)
+    return table, deck
+
+
+def river(table, deck):
+    deck = discard_a_card(deck)
+    card, deck = deal_a_card(deck)
     table.append(card)
-    return table, cards
+    return table, deck
+
+
+def get_possible_hands(table, player_hand):
+    cards = [player_hand[1], player_hand[2]]
+    hands = []
+    combinations = list(itertools.combinations(table, 3))
+    # print(combinations)
+    for combo in combinations:
+        hands.append(cards + list(combo))
+    return hands
+
+
+def best_player_hand(table, player_hand):
+    possible_hands = get_possible_hands(table, player_hand)
+    # print(possible_hands)
+    scores = []
+    for hand in possible_hands:
+        scores.append([hand, score_hand(hand)[0], score_hand(hand)[1]])
+    # print(scores)
+    scores.sort(key=lambda x: x[1])
+    if len(scores) == 1:
+        best_hand = scores[0]
+    else:
+        if scores[-1][1] != scores[-2][1]:
+            best_hand = scores[-1]
+        else:
+            highest_ranks = [score[1] == scores[-1][1] for score in scores]
+            highest_ranks.sort(key=lambda x: ranks.index(x))
+            if highest_ranks[0][2] == 'Ace':
+                best_hand = highest_ranks[0]
+            else:
+                best_hand = highest_ranks[-1]
+    return best_hand[0], best_hand[1]
+
+
+def get_player_scores(table, player_hands):
+    scores = {}
+    for player_hand in player_hands:
+        best_hand, score = best_player_hand(table, player_hand)
+        scores[player_hand[0]] = [best_hand, score]
+    return scores  # {player_number: best_hand, score_of_best_hand}
 
 
 def play(num_of_players):
     hands, cards = deal_to_players(num_of_players)
-    table, cards = deal_to_table(cards)
-    return hands, table, len(cards)
+    table, cards = flop(cards)
+    scores = get_player_scores(table, hands)
+    table, cards = turn(table, cards)
+    table, cards = river(table, cards)
+    return hands, table, scores
 
 
 print(play(3))
-
+# print(get_possible_hands([('10', 'Clubs'), ('8', 'Clubs'), ('7', 'Hearts'), ('Jack', 'Spades')], [1, ('King', 'Hearts'), ('5', 'Spades')]))
+# print(best_player_hand([('10', 'Clubs'), ('8', 'Clubs'), ('7', 'Hearts'), ('Jack', 'Spades')], [1, ('King', 'Hearts'), ('5', 'Spades')]))
+# print(best_player_hand([('10', 'Clubs'), ('8', 'Clubs'), ('Queen', 'Hearts'), ('Jack', 'Spades')], [1, ('King', 'Hearts'), ('9', 'Spades')]))
